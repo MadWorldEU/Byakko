3659d2e2a3c4828cac797b78a7e56dc605708b0b
oveldman <oveldman@gmail.com>
2026-04-28 13:19:55 +0200

    Integrate Admin, Api, and Portal projects into Aspire solution and configure AppHost to initialize them

--- src/MadWorldEU.Byakko.Aspire/AppHost.cs
+++ src/MadWorldEU.Byakko.Aspire/AppHost.cs
@@ -1,3 +1,9 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Admin>(nameof(Admin));
+builder.AddProject<Api>(nameof(Api));
+builder.AddProject<Portal>(nameof(Portal));
 
 builder.Build().Run();
