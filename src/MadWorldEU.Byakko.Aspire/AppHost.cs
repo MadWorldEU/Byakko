bf7226bf7bbe3484ae86d806dd4d7545d2fd7c8c
oveldman <oveldman@gmail.com>
2026-05-01 17:48:06 +0200

    Add PostgreSQL support to AppHost with database and dependencies

--- src/MadWorldEU.Byakko.Aspire/AppHost.cs
+++ src/MadWorldEU.Byakko.Aspire/AppHost.cs
@@ -2,8 +2,23 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Admin>(nameof(Admin));
-builder.AddProject<Api>(nameof(Api));
-builder.AddProject<Portal>(nameof(Portal));
+var username = builder.AddParameter("username", secret: true);
+var password = builder.AddParameter("password", secret: true);
+
+var postgres = builder.AddPostgres("postgres", username, password)
+    .WithDataVolume(isReadOnly: false)
+    .WithPgAdmin();
+
+var byakkoDb = postgres.AddDatabase("byakko-db");
+
+var api = builder.AddProject<Api>(nameof(Api))
+    .WaitFor(byakkoDb)
+    .WithReference(byakkoDb);
+
+builder.AddProject<Admin>(nameof(Admin))
+    .WaitFor(api);
+
+builder.AddProject<Portal>(nameof(Portal))
+    .WaitFor(api);
 
 builder.Build().Run();
