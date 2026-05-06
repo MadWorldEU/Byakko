bdd0518152ac2df3df80e764eae1b868bc460cfe
oveldman <oveldman@gmail.com>
2026-05-06 22:09:55 +0200

    Add health check files for Admin and Portal, update API to include health checks

--- src/MadWorldEU.Byakko.Controller.Api/Program.cs
+++ src/MadWorldEU.Byakko.Controller.Api/Program.cs
@@ -2,9 +2,7 @@ using MadWorldEU.Byakko.Endpoints.Development;
 using MadWorldEU.Byakko.Endpoints.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
 
