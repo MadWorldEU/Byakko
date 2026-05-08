e414d74097c39e08b20d84e6b395bc81b88da3c9
oveldman <oveldman@gmail.com>
2026-05-09 00:28:09 +0200

    Add initial Entity Framework migration and update dependencies: - Create `InitialMigration` to define `Assets` table schema. - Update `Directory.Packages.props` and `Api.csproj` with `Microsoft.EntityFrameworkCore.Design`. - Modify `AssetEntityTypeConfiguration` to include value conversion for `Name`.

---
+++ src/MadWorldEU.Byakko.Infrastructure.Postgresql/Migrations/20260508222403_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MadWorldEU.Byakko.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assets");
+        }
+    }
+}
--- src/MadWorldEU.Byakko.Infrastructure.Postgresql/Storages/AssetEntityTypeConfiguration.cs
+++ src/MadWorldEU.Byakko.Infrastructure.Postgresql/Storages/AssetEntityTypeConfiguration.cs
@@ -7,9 +7,10 @@ public class AssetEntityTypeConfiguration : IEntityTypeConfiguration<Asset>
     public void Configure(EntityTypeBuilder<Asset> builder)
     {
         builder.HasKey(a => a.Id);
-        
+
         builder.Property(a => a.Name)
             .IsRequired()
-            .HasMaxLength(Name.MaxLength);
+            .HasMaxLength(Name.MaxLength)
+            .HasConversion<string>(n => n.Value, s => Name.Create(s).Value);
     }
 }
