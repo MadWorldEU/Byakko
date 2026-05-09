a67e60433a3a820bf1736f733e8ff7d0a6e63415
oveldman <oveldman@gmail.com>
2026-05-09 12:32:23 +0200

    Introduce Testcontainers for PostgreSQL in integration tests: - Add `Testcontainers.PostgreSql` package to manage test containers. - Update `ApiHooks` to configure and use PostgreSQL container for integration tests. - Update `Directory.Packages.props`, project references, and global usings.

--- tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/GlobalUsings.cs
+++ tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/GlobalUsings.cs
@@ -1,4 +1,6 @@
 global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.Extensions.DependencyInjection;
 global using Reqnroll;
-global using System.Net;
 global using Shouldly;
+global using System.Net;
+global using Testcontainers.PostgreSql;
--- tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/Hooks/ApiHooks.cs
+++ tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/Hooks/ApiHooks.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace MadWorldEU.Byakko.Hooks;
 
 [Binding]
 public sealed class ApiHooks
 {
+    private static PostgreSqlContainer _postgres = null!;
     private static WebApplicationFactory<Program>? _factory;
     private static HttpClient? _client;
 
@@ -14,17 +17,38 @@ public sealed class ApiHooks
     }
 
     [BeforeTestRun]
-    public static void BeforeTestRun()
+    public static async Task BeforeTestRun()
     {
-        _factory = new WebApplicationFactory<Program>();
+        _postgres = new PostgreSqlBuilder().Build();
+        await _postgres.StartAsync();
+
+        _factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(host =>
+            {
+                host.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<ByakkoContext>));
+                    if (descriptor is not null)
+                        services.Remove(descriptor);
+
+                    services.AddDbContext<ByakkoContext>(options =>
+                        options.UseNpgsql(_postgres.GetConnectionString()));
+                });
+            });
+
+        using var scope = _factory.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ByakkoContext>();
+        await context.Database.MigrateAsync();
+
         _client = _factory.CreateClient();
     }
 
     [AfterTestRun]
-    public static void AfterTestRun()
+    public static async Task AfterTestRun()
     {
         _client?.Dispose();
         _factory?.Dispose();
+        await _postgres.DisposeAsync();
     }
 
     [BeforeScenario]
