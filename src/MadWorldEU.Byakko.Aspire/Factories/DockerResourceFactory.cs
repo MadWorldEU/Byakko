238b286e0369f51cebd27ef33f87cabd6ddfde7b
oveldman <oveldman@gmail.com>
2026-05-17 18:00:12 +0200

    Update to disable `UseDockerFile`, switch containers to user execution, and standardize HTTP port to 8080.

--- src/MadWorldEU.Byakko.Aspire/Factories/DockerResourceFactory.cs
+++ src/MadWorldEU.Byakko.Aspire/Factories/DockerResourceFactory.cs
@@ -12,14 +12,14 @@ internal sealed class DockerResourceFactory(IDistributedApplicationBuilder build
     public IResourceBuilder<IResource> CreateAdminBuilder(IResourceBuilder<IResource> api)
     {
         return builder.AddDockerfile(nameof(Admin), "../../", "src/MadWorldEU.Byakko.Controller.Admin/Dockerfile")
-            .WithHttpEndpoint(targetPort: 80)
+            .WithHttpEndpoint(targetPort: 8080)
             .BuildAdmin(api);
     }
 
     public IResourceBuilder<IResource> CreatePortalBuilder(IResourceBuilder<IResource> api)
     {
         return builder.AddDockerfile(nameof(Portal), "../../", "src/MadWorldEU.Byakko.Controller.Portal/Dockerfile")
-            .WithHttpEndpoint(targetPort: 80)
+            .WithHttpEndpoint(targetPort: 8080)
             .BuildPortal(api);
     }
 }
