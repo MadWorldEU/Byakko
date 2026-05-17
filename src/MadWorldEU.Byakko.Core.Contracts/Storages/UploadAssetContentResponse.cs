a7c48c4d00df4be6c20f112f6cb4279a7f9a3db8
oveldman <oveldman@gmail.com>
2026-05-17 18:18:38 +0200

    Refactor `UploadAssetContentResponse` to record and fix Dockerfile variable quoting for consistency.

--- src/MadWorldEU.Byakko.Core.Contracts/Storages/UploadAssetContentResponse.cs
+++ src/MadWorldEU.Byakko.Core.Contracts/Storages/UploadAssetContentResponse.cs
@@ -1,6 +1,4 @@
 namespace MadWorldEU.Byakko.Storages;
 
 /// <summary>Response returned after successfully uploading binary content for an asset.</summary>
-public sealed class UploadAssetContentResponse
-{
-}
+public sealed record UploadAssetContentResponse;
