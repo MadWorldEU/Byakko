5ebca892c925792e5c8efb8d40c92db18bf20ede
oveldman <oveldman@gmail.com>
2026-05-27 10:16:19 +0200

    Add `UpdatedAt` and `ExpiresAt` to asset metadata and adjust related tests and contracts

--- src/MadWorldEU.Byakko.Core.Contracts/Storages/GetAssetMetadataResponse.cs
+++ src/MadWorldEU.Byakko.Core.Contracts/Storages/GetAssetMetadataResponse.cs
@@ -6,5 +6,7 @@ public sealed class GetAssetMetadataResponse
     public required Guid Id { get; init; }
     public required string Name { get; init; }
     public required string ContentType { get; init; }
-    public required string CreatedAt { get; init; }
+    public required DateTimeOffset CreatedAt { get; init; }
+    public required DateTimeOffset UpdatedAt { get; init; }
+    public required DateTimeOffset ExpiresAt { get; init; }
 }
