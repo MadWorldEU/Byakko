208b52d6bf3473bde69d462dbb40273a4c6a39bf
oveldman <oveldman@gmail.com>
2026-05-30 12:18:40 +0200

    Fix parameter name in IEncryptionService.Decrypt(Stream) for clarity

--- src/MadWorldEU.Byakko.Core.Domain/Encryptions/IEncryptionService.cs
+++ src/MadWorldEU.Byakko.Core.Domain/Encryptions/IEncryptionService.cs
@@ -3,7 +3,7 @@ namespace MadWorldEU.Byakko.Encryptions;
 public interface IEncryptionService
 {
     string Decrypt(string encryptedContent);
-    Stream Decrypt(Stream content);
+    Stream Decrypt(Stream encryptedContent);
     string Encrypt(string content);
     Stream Encrypt(Stream content);
 }
