c3f224a8728d9fbdad37c84c30eccf49a2f182e1
oveldman <oveldman@gmail.com>
2026-06-06 15:08:08 +0200

    Add integration test for retrieving paged assets as an administrator

--- tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/Features/Storages/Assets.feature
+++ tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/Features/Storages/Assets.feature
@@ -1,5 +1,12 @@
 Feature: Asset Storage Endpoints
 
+Scenario: Retrieve two assets as an administrator
+    Given I am authenticated as an administrator
+    And I have created 2 assets
+    When I request page 1 of all assets
+    Then the response status code should be 200
+    And the response should contain the created assets
+
 Scenario: Upload and download a file for an asset
     Given I am authenticated as a user
     And I have created an asset with name "test-file.txt" and content type "text/plain"
--- tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/StepDefinitions/Storages/AssetsSteps.cs
+++ tests/MadWorldEU.Byakko.Controller.Api.IntegrationTests/StepDefinitions/Storages/AssetsSteps.cs
@@ -4,12 +4,61 @@ namespace MadWorldEU.Byakko.StepDefinitions.Storages;
 public sealed class AssetsSteps(ScenarioContext scenarioContext)
 {
     private const string AssetIdKey = "AssetId";
+    private const string CreatedAssetIdsKey = "CreatedAssetIds";
 
     [Given("I am authenticated as a user")]
     public void GivenIAmAuthenticatedAsAUser()
     {
         var authenticatedClient = scenarioContext.Get<HttpClient>(ScenarioContextKeys.AuthenticatedClient);
         scenarioContext.Set(authenticatedClient);
+    }
+
+    [Given("I am authenticated as an administrator")]
+    public void GivenIAmAuthenticatedAsAnAdministrator()
+    {
+        var authenticatedClient = scenarioContext.Get<HttpClient>(ScenarioContextKeys.AuthenticatedClient);
+        scenarioContext.Set(authenticatedClient);
+    }
+
+    [Given("I have created {int} assets")]
+    public async Task GivenIHaveCreatedAssets(int count)
+    {
+        var client = scenarioContext.Get<HttpClient>();
+        var ids = new List<Guid>();
+
+        for (var i = 0; i < count; i++)
+        {
+            var request = new CreateAssetRequest { Name = $"file-{i + 1}.txt", ContentType = "text/plain" };
+            var response = await client.PostAsJsonAsync("/assets", request);
+            response.EnsureSuccessStatusCode();
+            var createResponse = await response.Content.ReadFromJsonAsync<CreateAssetResponse>();
+            ids.Add(createResponse!.Id);
+        }
+
+        scenarioContext.Set(ids, CreatedAssetIdsKey);
+    }
+
+    [When("I request page {int} of all assets")]
+    public async Task WhenIRequestPageOfAllAssets(int page)
+    {
+        var client = scenarioContext.Get<HttpClient>();
+        var response = await client.GetAsync($"/assets?page={page}");
+        scenarioContext.Set(response, ScenarioContextKeys.LastResponse);
+    }
+
+    [Then("the response should contain the created assets")]
+    public async Task ThenTheResponseShouldContainTheCreatedAssets()
+    {
+        var body = await scenarioContext.Get<HttpResponseMessage>(ScenarioContextKeys.LastResponse)
+            .Content.ReadFromJsonAsync<GetAssetsMetadataResponse>();
+
+        var createdIds = scenarioContext.Get<List<Guid>>(CreatedAssetIdsKey);
+        body.ShouldNotBeNull();
+        body.TotalCount.ShouldBeGreaterThanOrEqualTo(createdIds.Count);
+        foreach (var id in createdIds)
+        {
+            body.Assets.ShouldContain(a => a.Id == id);
+        }
     }
 
     [Given("I have created an asset with name {string} and content type {string}")]
