79116d4ada2117d69f16c8a9a23d850c3dadfd21
oveldman <oveldman@gmail.com>
2026-06-20 11:51:01 +0200

    Add country flag support for language selection in layout and localization

--- src/MadWorldEU.Byakko.Controller.Portal/Localization/SupportedLanguages.cs
+++ src/MadWorldEU.Byakko.Controller.Portal/Localization/SupportedLanguages.cs
@@ -4,11 +4,18 @@ public static class SupportedLanguages
 {
     public const string DefaultLanguage = "en";
     public const string DefaultUiLanguage = "EN";
-    
+
     public static Dictionary<string, string> Labels { get; } = new()
     {
         { DefaultLanguage, DefaultUiLanguage },
         { "nl-NL", "NL" },
         { "ja-JP", "JA" }
+    };
+
+    public static Dictionary<string, string> Flags { get; } = new()
+    {
+        { DefaultLanguage, "🇬🇧" },
+        { "nl-NL", "🇳🇱" },
+        { "ja-JP", "🇯🇵" }
     };
 }
