534f9b4c69cce1d560a156cbb7f282461e9f5b21
oveldman <oveldman@gmail.com>
2026-06-21 17:07:33 +0200

    Add shared localization resources and update NotFound page to use dynamic localization

--- src/MadWorldEU.Byakko.Controller.Portal/Program.cs
+++ src/MadWorldEU.Byakko.Controller.Portal/Program.cs
@@ -11,7 +11,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddLocalization();
 
 builder.AddByakkoApiHttpClients();
 builder.AddByakkoAuthentication();
