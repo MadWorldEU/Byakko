d4bcf95faf087ec5faa52b4a483356e68aeeb0d4
oveldman <oveldman@gmail.com>
2026-07-02 09:59:01 +0200

    Update Keycloak to v26.5 and Postgres to v17 in Helm values and integration tests

--- tests/MadWorldEU.Byakko.Controller.Status.IntegrationTests/Common/DockerImages.cs
+++ tests/MadWorldEU.Byakko.Controller.Status.IntegrationTests/Common/DockerImages.cs
@@ -4,5 +4,5 @@ internal static class DockerImages
 {
     internal const string LocalStack = "localstack/localstack:2.0";
     internal const string Mailpit = "axllent/mailpit:v1.30";
-    internal const string Postgres = "postgres:15.1";
+    internal const string Postgres = "postgres:17";
 }
