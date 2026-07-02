66153d1583a4efa479dfb2bd69e48e22099bc564
oveldman <oveldman@gmail.com>
2026-07-02 22:54:22 +0200

    Add migration for `Accounts` table with schema for account management

---
+++ src/MadWorldEU.Byakko.Infrastructure.Postgresql/Migrations/20260702205408_AddAccountInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace MadWorldEU.Byakko.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    HasDeletionRequested = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<Instant>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<Instant>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
